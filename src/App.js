190127043be0b8cee05180e20b9fc67da15bbc62
gowthammajeti9 <132914099+gowthammajeti9@users.noreply.github.com>
2024-10-10 10:36:02 -0500

    removed the comments

--- src/App.js
+++ src/App.js
@@ -1,31 +1,4 @@
 
-// // src/App.js
-// import React from 'react';
-// import RegistrationForm from './RegistrationForm';
-// import MembersTable from './MembersTable';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>Welcome to JBoss!</h1>
-//       <p>You have successfully deployed a Jakarta EE Enterprise Application.</p>
-
-//       <div className="registration">
-//         <h2>Member Registration</h2>
-//         <RegistrationForm />
-//       </div>
-
-//       <div className="members">
-//         <h2>Members</h2>
-//         <MembersTable />
-//       </div>
-
-//       {/* Additional content and footer */}
-//     </div>
-//   );
-// }
-
-// export default App;
 
 import React from 'react';
 import './App.css';
